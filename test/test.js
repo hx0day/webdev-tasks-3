8e88b90e341c259700641220320296c59bd5e2f2
hx0day <hx0day@hackerdom.ru>
2016-03-10 01:18:10 +0500

    feat(*): add all test

--- test/test.js
+++ test/test.js
@@ -3,70 +3,73 @@ var assert = require('assert');
 // Ваша супер библиотека
 var flow = require('../lib/flow.js');
 
-describe('Check', function () {
 
-    it('should check serial', function (done) {
+describe('Check serial', function () {
+
+    it('should check', function (done) {
         var result = [];
 
         function callback(error, data) {
             result = data;
-            assert.deepEqual(result, [2, 1]);
+            console.log(result);
+            assert.equal(result, 3);
             done();
         }
 
-        function test2(callback) {
+        function test2(data, callback) {
             setTimeout(function () {
-                callback(false, 1);
-            }, 0);
+                callback(false, data + 2);
+            }, 300);
 
         }
 
         function test1(callback) {
             setTimeout(function () {
-                callback(false, 2);
-            }, 0);
+                callback(false, 1);
+            }, 100);
         }
 
         var test_function = [test1, test2];
-        flow.parallel(test_function, callback);
+        flow.serial(test_function, callback);
 
     });
 
-    it('should check serial error', function (done) {
+    it('should check error', function (done) {
         var result = [];
 
         function callback(error, data) {
             result = data;
-            assert.deepEqual(result, [2]);
+            assert.deepEqual(result, 3);
             assert.ok(error);
             done();
         }
 
-        function test3(callback) {
+        function test3(data, callback) {
             setTimeout(function () {
-                callback(false, 1);
+                callback(false, data + 1);
             }, 0);
 
         }
-        function test2(callback) {
+
+        function test2(data, callback) {
             setTimeout(function () {
-                callback(true, 1);
-            }, 0);
+                callback(true, data + 2);
+            }, 300);
 
         }
 
         function test1(callback) {
             setTimeout(function () {
-                callback(false, 2);
-            }, 0);
+                callback(false, 1);
+            }, 100);
         }
 
+
         var test_function = [test1, test2, test3];
-        flow.parallel(test_function, callback);
+        flow.serial(test_function, callback);
 
     });
-
-    it('should check emptyArray in serial', function (done) {
+    it('should check emptyArray', function (done) {
         var result = [];
 
         function callback(error, data) {
@@ -77,9 +80,11 @@ describe('Check', function () {
         var test_function = [];
         flow.serial(test_function, callback);
     });
+});
+describe('Check parallel', function () {
 
 
-    it('should check parallel one error', function (done) {
+    it('should check error', function (done) {
         var result = [];
 
 
@@ -133,7 +138,7 @@ describe('Check', function () {
 
     });
 
-    it('should check emptyArray in parallel', function (done) {
+    it('should check emptyArray', function (done) {
         var result = [];
 
         function callback(error, data) {
@@ -144,30 +149,63 @@ describe('Check', function () {
         var test_function = [];
         flow.parallel(test_function, callback);
     });
+});
+describe('Check map', function () {
+    it('should check', function (done) {
+        var values = [5, 2];
+
+        function callback(error, data) {
+            assert.deepEqual(error, false);
+            assert.deepEqual(data, values);
+            done();
+        }
 
 
-    it('should check emptyArray in map', function (done) {
-        var result = [];
+        function test_function(val, callback) {
+            setTimeout(function () {
+                callback(false, val);
+            }, 0);
+        }
+
+        flow.map(values, test_function, callback);
+    });
+
+    it('should error', function (done) {
+        var values = [5, 2, 3];
+
+        function callback(error, data) {
+            assert.deepEqual(error, true);
+            assert.deepEqual(data, [5]);
+            done();
+        }
+
+
+        function test_function(val, callback) {
+            setTimeout(function () {
+                var error = false;
+                if (val === 2) {
+                    error = true;
+                }
+                callback(error, val);
+            }, 0);
+        }
+
+        flow.map(values, test_function, callback);
+    });
+
+    it('should check emptyArray', function (done) {
 
         function callback(error, data) {
             assert.deepEqual(error, null);
             done();
         }
 
-        function test2(callback) {
+        function test_function(val, callback) {
             setTimeout(function () {
-                callback(false, 1);
-            }, 0);
-
-        }
-
-        function test1(callback) {
-            setTimeout(function () {
-                callback(false, 2);
+                callback(false, val);
             }, 0);
         }
 
-        var test_function = [test1, test2];
         flow.map([], test_function, callback);
     });
 });
